78d222ad0bd8722954cc6c0aa8fe0744e9b023b7
unknown <np03cs4s220225@heraldcollege.edu.np>
2023-07-21 09:15:40 +0545

    CR done UD left

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Blog
+from .forms import forms
+from .forms import BlogForm
+
 # Create your views here.
 def homepage(request):
-    return render(request,"crud/index.html")
+    blog = Blog.objects.all()
+    print(blog)
+    return render(request,"crud/index.html", {"Blogs": blog})
+
+def create(request):
+    forms = BlogForm(request.POST or None)
+    if(forms.is_valid()):
+        forms.save()
+        return redirect("home")
+    return render(request, "crud/create.html", {"forms": forms})
